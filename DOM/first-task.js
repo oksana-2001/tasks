50c6f237308a8dac1b9540d1cd6104c3575d68d2
Oksana Semenchenko <vesna782006@yandex.ru>
2021-05-16 20:58:43 +0300

    задача слайдер

---
+++ DOM/first-task.js
@@ -0,0 +1,52 @@
+const string = "Привет! Как дела?";
+
+const vowels = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я"];
+
+const getVowels = stringToFilter => {
+	let extractedVowels = "";
+	
+	for (let i = 0; i < stringToFilter.length; i++){
+		const currentLetter = stringToFilter[i].toLowerCase();
+
+		if (vowels.includes(currentLetter) ) {
+			extractedVowels += currentLetter;
+		}
+	}
+	
+	return extractedVowels;
+}
+
+console.log(getVowels(string));
+
+
+const workers = [
+	{"name":"John","salary":500},
+	{"name":"Mike","salary":1300},
+	{ "name": "Linda", "salary": 1500 }
+];
+
+const getWorthyWorkers = (workersArr) => {
+	const WorthyWorkers = [];
+
+	workersArr.forEach(currentWorker => {
+		if (currentWorker.salary > 1000) {
+			WorthyWorkers.push(currentWorker.name)
+		}
+	})
+	return WorthyWorkers;
+}
+  console.log(getWorthyWorkers(workers))
+
+
+
+
+const path = "/users/download/index.html"
+
+const isHtml  = path => {
+	const requiredExt = ".html";
+	const pathExt = path.slice(-5);
+
+	return pathExt === requiredExt;
+}
+
+console.log(isHtml(path));
